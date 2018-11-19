b13fe616d083d683af5c5fb56802f9ad6a44b4ea
ezra davis davis <ezra.rr.davis@gmail.com>
2018-11-18 23:28:54 -0800

    firebase functions init

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,5 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const server = require('../server.js');
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.server = functions.https.onRequest(server);
--- server.js
+++ server.js
@@ -2,4 +2,5 @@ const express = require('express');
 const serverRouters = require('./routers/serverRouters.js');
 const server = express();
 
+server.use('/', serverRouters);
 module.exports = server;
